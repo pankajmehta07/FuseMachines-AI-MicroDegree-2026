768422eab364a14090bbc6053a4b7f42ffe4373a
pankajmehta07 <123pankajmehta@gmail.com>
2026-05-08 16:36:03 +0545

    made pydantic and sqlalchemy models and database connection function

---
+++ Week 2/postgresDocker/database.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+load_dotenv()
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+db_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+print(db_url)
+
+try:
+    engine = create_engine(db_url)
+    print("DB engine created successfully")
+except:
+    print("Failed to create DB engine")
+
+
+
+session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = session()
+    try:
+        print("DB session opened")
+        yield db
+    
+    finally:
+        db.close()
+        print("DB session closed ")
---
+++ Week 2/postgresDocker/schemas.py
@@ -0,0 +1,88 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from datetime import date
+from decimal import Decimal
+
+class orderDetails(BaseModel):
+    orderNumber: int
+    productCode: str
+    quantityOrdered: int
+    priceEach: Decimal
+    orderLineNumber: int
+
+    class Config:
+        from_attributes = True
+
+class orderOut(BaseModel):
+    orderNumber: int
+    orderDate: date
+    requiredDate: date
+    shippedDate: Optional[date] = None
+    status: str
+    comments: Optional[str]
+    customerNumber: int
+
+    class Config:
+        from_attributes = True
+
+class paymentOut(BaseModel):
+    customerNumber: int
+    checkNumber: str
+    paymentDate: date
+    amount: Decimal
+
+    class Config:
+        from_attributes = True
+
+
+class customerCreate(BaseModel):
+    customerNumber: int
+    customerName: str
+    contactLastName:str
+    contactFirstName:str
+    phone: str
+    addressLine1: str
+    addressLine2: Optional[str] = None
+    city: str
+    state: Optional[str] = None
+    postalCode: Optional[str] = None
+    country: str
+    salesRepEmployeeNumber: Optional[int] = None
+    creditLimit: Optional[Decimal] = None
+
+class customerOut(BaseModel):
+    customerNumber: int
+    customerName: str
+    contactLastName:str
+    contactFirstName:str
+    phone: str
+    addressLine1: str
+    addressLine2: Optional[str] = None
+    city: str
+    state: Optional[str] = None
+    postalCode: Optional[str] = None
+    country: str
+    salesRepEmployeeNumber: Optional[int] = None
+    creditLimit: Optional[Decimal] = None
+    # orders: List(orderOut) = []
+    # payments: List(paymentOut) = []
+
+
+    class Config:
+        from_attributes = True
+
+class customerUpdate(BaseModel):
+    customerName: Optional[str] = None
+    contactLastName:Optional[str] = None
+    contactFirstName:Optional[str] = None
+    phone: Optional[str] = None
+    addressLine1: Optional[str] = None
+    addressLine2: Optional[str] = None
+    city: Optional[str] = None
+    state: Optional[str] = None
+    postalCode: Optional[str] = None
+    country: Optional[str] = None
+    salesRepEmployeeNumber: Optional[int] = None
+    creditLimit: Optional[Decimal] = None
+
+print("Schemas Loaded successfully")
